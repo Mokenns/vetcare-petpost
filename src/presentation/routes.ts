551d75ea8b80840955b7758a8167b26e3818336a
Mokens <miquiangelhotmail@gmail.com>
2025-03-17 19:38:09 -0300

    enabled petpost routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { UserRoutes } from './users/routes';
+import { PetPostRoutes } from './petPost/routes';
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.use('/api/users', UserRoutes.routes);
-    //router.use('/api/petposts', PetPostRoutes.routes);
+    router.use('/api/petposts', PetPostRoutes.routes);
 
     return router;
   }
